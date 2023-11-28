8295565b0f858144e567fa2da1c3c6c624095ada
Kimihito <lambent.darkness@mail.ru>
2023-11-28 06:05:35 +0300

    Output changes

--- EllipticCurveEncryption/src/main.cpp
+++ EllipticCurveEncryption/src/main.cpp
@@ -4,8 +4,8 @@ int main() {
     EllipticCurveEncryption encryption;
     encryption.Encrypt();
     cout << "Encryption:" << endl;
-    cout << encryption.encryptedPoints[0].x << " " << encryption.encryptedPoints[0].y << endl;
-    cout << encryption.encryptedPoints[1].x << " " << encryption.encryptedPoints[1].y << endl;
+    cout << "C1: (x:" << encryption.encryptedPoints[0].x << " y:" << encryption.encryptedPoints[0].y << ")" << endl;
+    cout << "C2: (x:" << encryption.encryptedPoints[1].x << " y:" << encryption.encryptedPoints[1].y << ")" << endl;
     cout << "Decryption:" << endl;
     vector<unsigned char> decryptedMessage = encryption.Decrypt();
     cout << string(decryptedMessage.begin(), decryptedMessage.end()) << endl;
